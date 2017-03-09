99ce99d295adcfa9828dd9c6a83e9083cca71520
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.1.130>
2017-03-09 19:38:30 -0300

    teste

---
+++ src/br/edu/opet/github/ClasseOlaMundo.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package br.edu.opet.github;
+
+/**
+ * @author Lab. Desenvolvimento
+ *
+ */
+public class ClasseOlaMundo
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Olá MUndoo");
+
+
+    }
+
+}
